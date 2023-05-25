5df56f779c4ea02c09c537532d33919562f1ebb0
durgeshahire07 <durgeshahire07@gmail.com>
2023-05-26 00:40:51 +0530

    feat: :construction: setup i18n for eng and hindi

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,22 +1,33 @@
-import React, { useContext } from 'react';
-import { View, Text, Button } from 'react-native';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { View, Text, Button } from "react-native";
+import { AppContext } from "../context/AppContext";
+import { useTranslation } from "react-i18next";
 
 const HomeScreen = ({ navigation }) => {
+  const { language, changeLanguage } = useContext(AppContext);
   const { user, setUser } = useContext(AppContext);
+  const { t } = useTranslation();
 
   const handleLogout = () => {
     setUser(null);
   };
 
+  const handleLanguageChange = () => {
+    const newLanguage = language === "en" ? "hi" : "en";
+    changeLanguage(newLanguage);
+  };
+
   return (
     <View>
-      <Text>Welcome to the Home Screen, {user ? user.name : 'Guest'}!</Text>
       {user && <Button title="Logout" onPress={handleLogout} />}
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+      <View>
+        <Text>{t("welcome")}</Text>
+        <Button title="Change Language" onPress={handleLanguageChange} />
+        <Button
+          title={t("details")}
+          onPress={() => navigation.navigate("Details")}
+        />
+      </View>
     </View>
   );
 };
