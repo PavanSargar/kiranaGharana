9349823f3f2147a1c008e6bbd5b2aaa58fee0c95
Pavan <pavansargar36@gmail.com>
2023-05-25 23:31:20 +0530

    inital

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Hello Akshay!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 50
+  }
 });
