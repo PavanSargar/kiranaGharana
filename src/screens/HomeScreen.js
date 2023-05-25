df3cfd8169c87787ace14a1a1cd3622787ab9f68
durgeshahire07 <durgeshahire07@gmail.com>
2023-05-26 01:26:47 +0530

    fix: :bug: merge conflicts

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,8 +11,6 @@ import {
 } from "react-native";
 import { AppContext } from "../context/AppContext";
 
-import { useTranslation } from "react-i18next";
-
 const DATA = [
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
